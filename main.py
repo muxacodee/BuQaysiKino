6b9badffdb0e34ad158d0809f8bd3a4cd8b55c37
muxacodee <icosoft@mail.ru>
2024-09-08 23:11:36 +0500

    Initial

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from aiogram.enums import ParseMode
 from data.config import API_TOKEN
 from data.db import Database
 from handlers import main_router
-import threading
+from aiohttp import web
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -26,20 +26,21 @@ async def start_bot():
     await dp.start_polling(bot)
 
 
-def run_bot():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(start_bot())
-
-
 @app.route('/')
 def index():
     return "Bot is running"
 
 
-if __name__ == '__main__':
-    bot_thread = threading.Thread(target=run_bot)
-    bot_thread.start()
+async def init_app():
+    # Start the Flask app
+    runner = web.AppRunner(web.Application())
+    await runner.setup()
+    site = web.TCPSite(runner, host='0.0.0.0', port=8000)
+    await site.start()
 
-    # Start Flask app
-    app.run(host='0.0.0.0', port=8000, debug=True)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(init_app())
+    loop.run_until_complete(start_bot())
+    loop.run_forever()
